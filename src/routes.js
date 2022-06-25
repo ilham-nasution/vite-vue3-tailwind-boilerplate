7b6458e42bfa4c408f05ed15bbe457166a888dcc
ilham-nasution <ilhamnasution@Admins-MacBook-Air-3.local>
2022-06-25 12:56:07 +0700

    initial setup

---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import Home from './pages/Home.vue'
+
+/** @type {import('vue-router').RouterOptions['routes']} */
+export const routes = [
+  { path: '/', component: Home, meta: { title: 'Home' } },
+]
